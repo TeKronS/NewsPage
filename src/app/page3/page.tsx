4afe31a9d1ee32b0551516506aa843d9bf1f98ac
TeKronS <asper.tk93@gmail.com>
2024-02-18 16:50:16 -0400

    first commit

---
+++ src/app/page3/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function PAGE() {
+  return <main className={styles.main}>PAGE 3</main>;
+}
---
+++ src/components/page-sections/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>PAGINA DE MARIO</h1>
+    </header>
+  );
+}
