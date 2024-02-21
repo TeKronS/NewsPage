3f62fb057fe6bc5243c8db856ace6869e8a87b96
TeKronS <asper.tk93@gmail.com>
2024-02-21 16:02:33 -0400

    add header and footer phone styled

--- src/components/page-sections/Footer/Footer.tsx
+++ src/components/page-sections/Footer/Footer.tsx
@@ -1,5 +1,26 @@
 import styles from "./footer.module.css";
 
 export default function Footer() {
-  return <footer className={styles.footer}>PIE DE PAGINA</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.topContainer}>
+        <div className={styles.relleno}></div>
+        <div className={styles.addmeContainer}>
+          <div>
+            <span>Síguenos</span>
+          </div>
+          <div className={styles.socialContainer}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className={styles.relleno}></div>
+      </div>
+      <div className={styles.linkContainer}>
+        <span>Contáctanos</span>
+        <span>Sobre Nosotros</span>
+      </div>
+    </footer>
+  );
 }
--- src/components/page-sections/Header/Header.tsx
+++ src/components/page-sections/Header/Header.tsx
@@ -1,9 +1,23 @@
 import styles from "./header.module.css";
+import Burger from "@/img/Burger";
+import Search from "@/img/Search";
+import Image from "next/image";
+import icon from "@/img/favicon.png";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>PAGINA DE MARIO</h1>
+      <div className={styles.burger}>
+        <Burger />
+      </div>
+      <h1 className={styles.title}>
+        {/* <span>ENTRE</span>
+        <span>NOTICIAS</span> */}
+        <Image src={icon} width={480} height={480} alt="" />
+      </h1>
+      <div className={styles.search}>
+        <Search />
+      </div>
     </header>
   );
 }
